0054756c87b6a18547a7bc428cd3478f9158b465
Carl Leijonberg <carllei@kth.se>
2016-05-07 23:32:10 +0200

    Render Method added and BitmapFont imported.

--- core/src/com/pqbyte/coherence/MainMenuScreen.java
+++ core/src/com/pqbyte/coherence/MainMenuScreen.java
@@ -3,20 +3,39 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 /**
  * Created by Carl on 5/7/2016.
  */
 public abstract class MainMenuScreen implements Screen {
     final Coherence game;
-
     OrthographicCamera camera;
 
     public MainMenuScreen(final Coherence gam) {
+        
         game = gam;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+    }
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Coherence", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
     }
 
 }
