b6282162c0dac744886242b960a9509b1a7ccc60
Carl Leijonberg <carllei@kth.se>
2016-05-07 23:21:44 +0200

    MainMenuScreen class made abstract. A constructor has been added

--- core/src/com/pqbyte/coherence/MainMenuScreen.java
+++ core/src/com/pqbyte/coherence/MainMenuScreen.java
@@ -7,6 +7,16 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 /**
  * Created by Carl on 5/7/2016.
  */
-public class MainMenuScreen implements Screen {
+public abstract class MainMenuScreen implements Screen {
+    final Coherence game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Coherence gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
 
 }
