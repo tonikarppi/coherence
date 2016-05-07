67c1e9abf6c91a578e9fd57e12c3322cb37e0636
Carl Leijonberg <carllei@kth.se>
2016-05-07 23:07:48 +0200

    Create main menu class that implements screen

---
+++ core/src/com/pqbyte/coherence/MainMenuScreen.java
@@ -0,0 +1,12 @@
+package com.pqbyte.coherence;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by Carl on 5/7/2016.
+ */
+public class MainMenuScreen implements Screen {
+
+}
