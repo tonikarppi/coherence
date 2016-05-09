4b5dab1543ad3173b2c8eddbf917163110a5bbf4
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-09 14:46:24 +0200

    Add Box2D filter flag constants

--- core/src/com/pqbyte/coherence/Constants.java
+++ core/src/com/pqbyte/coherence/Constants.java
@@ -3,4 +3,8 @@ package com.pqbyte.coherence;
 public final class Constants {
   public static final int WORLD_WIDTH = 160;
   public static final int WORLD_HEIGHT = 90;
+
+  /* Mask flags for Box2D filters. */
+  public static final short PHYSICS_ENTITY = 0x1;
+  public static final short WORLD_ENTITY = 0x2;
 }
