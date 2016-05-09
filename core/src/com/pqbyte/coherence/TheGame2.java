6d46708859e52527c87829362aa51964a8c1dff8
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-09 14:48:40 +0200

    Implement Box2D API to TheGame2

--- core/src/com/pqbyte/coherence/TheGame2.java
+++ core/src/com/pqbyte/coherence/TheGame2.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
@@ -11,18 +13,26 @@ public class TheGame2 extends ApplicationAdapter {
   private static final float VIEWPORT_WIDTH = 30;
 
   private Stage stage;
-
+  private World world;
 
   @Override
   public void create() {
-    Player player = new Player(new Texture(Gdx.files.internal("cube128.png")), 0, 0);
-    player.addListener(new MoveListener(player));
+    world = new World(new Vector2(0, 0), true);
 
     Map map = new Map(
         new Texture(Gdx.files.internal("wallpaper.jpg")),
         Constants.WORLD_WIDTH,
-        Constants.WORLD_HEIGHT
+        Constants.WORLD_HEIGHT,
+        world
     );
+
+    Player player = new Player(
+        new Texture(Gdx.files.internal("cube128.png")),
+        0,
+        0,
+        world
+    );
+    player.addListener(new MoveListener(player));
 
     float screenWidth = Gdx.graphics.getWidth();
     float screenHeight = Gdx.graphics.getHeight();
@@ -39,6 +49,7 @@ public class TheGame2 extends ApplicationAdapter {
   @Override
   public void dispose() {
     stage.dispose();
+    world.dispose();
   }
 
   @Override
@@ -46,6 +57,7 @@ public class TheGame2 extends ApplicationAdapter {
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+    world.step(1 / 60f, 6, 2);
     stage.act(Gdx.graphics.getDeltaTime());
     stage.draw();
   }
