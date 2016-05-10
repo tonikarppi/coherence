412d08272c9d01fce1bd64324e783ac1ecc4ffbb
Carl Leijonberg <carllei@kth.se>
2016-05-10 09:56:22 +0200

    Add simple GameScreen. Change AndroidLauncher to start Coherence2 instead.

---
+++ core/src/com/pqbyte/coherence/GameScreen.java
@@ -0,0 +1,51 @@
+package com.pqbyte.coherence;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Carl on 5/9/2016.
+ */
+public class GameScreen implements Screen {
+    final Coherence2 game;
+
+
+    public GameScreen(final Coherence2 game) {
+        this.game = game;
+    }
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void show () {
+    }
+
+    @Override
+    public void hide () {
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void dispose () {
+    }
+
+    @Override
+    public void render(float delta){}
+}
+
+
--- core/src/com/pqbyte/coherence/MainMenuScreen.java
+++ core/src/com/pqbyte/coherence/MainMenuScreen.java
@@ -4,33 +4,37 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by Carl on 5/7/2016.
  */
-public abstract class MainMenuScreen implements Screen {
-    final Coherence game;
+public class MainMenuScreen implements Screen {
+    final Coherence2 game;
     OrthographicCamera camera;
 
-    public MainMenuScreen(final Coherence gam) {
-        
-        game = gam;
+    public MainMenuScreen(final Coherence2 game) {
+        this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
     }
+
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(135/255f, 206/255f, 235/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        SpriteBatch batch = game.batch;
+        BitmapFont font = game.font;
 
-        game.batch.begin();
-        game.font.draw(game.batch, "Coherence", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-        game.batch.end();
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        font.draw(batch, "Coherence", 100, 200);
+        font.draw(batch, "Tap anywhere to begin!", 100, 50);
+        batch.end();
 
         if (Gdx.input.isTouched()) {
             game.setScreen(new GameScreen(game));
@@ -38,4 +42,21 @@ public abstract class MainMenuScreen implements Screen {
         }
     }
 
+    @Override
+    public void show() { /* Do nothing */ }
+
+    @Override
+    public void resize(int width, int height) { /* Do nothing */ }
+
+    @Override
+    public void pause() { /* Do nothing */}
+
+    @Override
+    public void resume() { /* Do nothing */}
+
+    @Override
+    public void hide() { /* Do nothing */ }
+
+    @Override
+    public void dispose() { /* Do nothing */ }
 }
