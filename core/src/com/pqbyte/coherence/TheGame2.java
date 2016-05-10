ca7bbb08dab0a96e06f4b91f516316846f83d839
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-10 17:12:37 +0200

    Add projectile functionality to Player

--- core/src/com/pqbyte/coherence/TheGame2.java
+++ core/src/com/pqbyte/coherence/TheGame2.java
@@ -4,20 +4,29 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.Iterator;
 
 public class TheGame2 extends ApplicationAdapter {
   private static final float VIEWPORT_WIDTH = 30;
 
   private Stage stage;
   private World world;
+  private Box2DDebugRenderer debugRenderer;
+  private Array<Projectile> bulletToBeRemoved;
 
   @Override
   public void create() {
     world = new World(new Vector2(0, 0), true);
+    bulletToBeRemoved = new Array<Projectile>();
+    world.setContactListener(new CollisionListener(bulletToBeRemoved));
 
     Map map = new Map(
         new Texture(Gdx.files.internal("wallpaper.jpg")),
@@ -32,13 +41,14 @@ public class TheGame2 extends ApplicationAdapter {
         0,
         world
     );
-    player.addListener(new MoveListener(player));
+    player.addListener(new PlayerControlListener(player));
 
     float screenWidth = Gdx.graphics.getWidth();
     float screenHeight = Gdx.graphics.getHeight();
 
     stage = new Stage(new ExtendViewport(
         VIEWPORT_WIDTH, VIEWPORT_WIDTH * (screenHeight / screenWidth)));
+    stage.addListener(new ShootingListener(player));
     stage.addActor(map);
     stage.addActor(player);
     stage.setKeyboardFocus(player);
@@ -57,7 +67,8 @@ public class TheGame2 extends ApplicationAdapter {
     Gdx.gl.glClearColor(1, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    world.step(1 / 60f, 6, 2);
+    removeUsedBullets();
+    world.step(1f / 60f, 6, 2);
     stage.act(Gdx.graphics.getDeltaTime());
     stage.draw();
   }
