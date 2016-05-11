3eb2cce6aaa87a17a93e4734aa8f00f8be554429
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-11 16:42:14 +0200

    Change world size to 100x100

--- core/src/com/pqbyte/coherence/Constants.java
+++ core/src/com/pqbyte/coherence/Constants.java
@@ -7,8 +7,8 @@ public final class Constants {
     return DEBUG;
   }
 
-  public static final int WORLD_WIDTH = 160;
-  public static final int WORLD_HEIGHT = 90;
+  public static final int WORLD_WIDTH = 100;
+  public static final int WORLD_HEIGHT = 100;
 
   /* Mask flags for Box2D filters. */
   public static final short PHYSICS_ENTITY = 0x1;
