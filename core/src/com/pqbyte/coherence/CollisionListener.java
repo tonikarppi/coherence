c073d59fd2390efd81dd046d88980f9be89f8543
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-12 19:42:39 +0200

    Add bullet to player collision detection

--- core/src/com/pqbyte/coherence/CollisionListener.java
+++ core/src/com/pqbyte/coherence/CollisionListener.java
@@ -1,9 +1,12 @@
 package com.pqbyte.coherence;
 
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
@@ -18,15 +21,24 @@ public class CollisionListener implements ContactListener {
 
   @Override
   public void beginContact(Contact contact) {
-    Actor actorA = (Actor) contact.getFixtureA().getBody().getUserData();
-    Actor actorB = (Actor) contact.getFixtureB().getBody().getUserData();
+    Fixture fixA = contact.getFixtureA();
+    Fixture fixB = contact.getFixtureB();
+    Actor actorA = (Actor) fixA.getBody().getUserData();
+    Actor actorB = (Actor) fixB.getBody().getUserData();
+    Filter filterA = fixA.getFilterData();
 
-    if (actorA instanceof Projectile
-        && (actorB instanceof Map || actorB instanceof Obstacle)) {
-      bulletsToBeRemoved.add((Projectile) actorA);
-    } else if (actorB instanceof Projectile
-        && (actorA instanceof Map || actorA instanceof Obstacle)) {
+    if (actorB instanceof Projectile
+        && filterA.categoryBits == Constants.WORLD_ENTITY) {
       bulletsToBeRemoved.add((Projectile) actorB);
+    } else if (actorB instanceof Projectile
+        && actorA instanceof Player) {
+      Player target = (Player) actorA;
+      Projectile bullet = (Projectile) actorB;
+      if (!bullet.getShooter().equals(actorA)) {
+        // The player isn't shooting themselves, take damage.
+        bulletsToBeRemoved.add(bullet);
+        target.takeDamage(10);
+      }
     }
   }
 
@@ -37,7 +49,6 @@ public class CollisionListener implements ContactListener {
 
   @Override
   public void preSolve(Contact contact, Manifold oldManifold) {
-
   }
 
   @Override
