53ef0ceddaffd3e3c74785b1ae34760a69f67160
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-20 23:33:15 +0200

    Rewrite PauseMenuScreen to use stage

--- core/src/com/pqbyte/coherence/PauseMenuScreen.java
+++ core/src/com/pqbyte/coherence/PauseMenuScreen.java
@@ -6,48 +6,89 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class PauseMenuScreen extends ScreenAdapter {
 
-  final Coherence game;
-  OrthographicCamera camera;
+  private Stage stage;
+  private Coherence game;
 
-  public PauseMenuScreen(final Coherence game) {
+  public PauseMenuScreen(Coherence game) {
     this.game = game;
+    Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+    Button button = createButton(skin);
+    Label titleLabel = createTitleLabel(skin);
 
-    camera = new OrthographicCamera();
-    camera.setToOrtho(false, 800, 480);
+    stage = new Stage();
+    stage.addActor(button);
+    stage.addActor(titleLabel);
+
+    Gdx.input.setInputProcessor(stage);
   }
 
   @Override
   public void render(float delta) {
-    Gdx.gl.glClearColor(0.09f, 0.28f, 0.2f, 1);
+    Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    SpriteBatch batch = game.batch;
-    BitmapFont font = game.font;
+    stage.draw();
+  }
 
-    camera.update();
-    batch.setProjectionMatrix(camera.combined);
+  @Override
+  public void dispose() {
+    stage.dispose();
+  }
 
-    batch.begin();
-    font.draw(batch, "The game has been paused", 300, 350);
-    font.draw(batch, "Tap anywhere to resume!", 300, 250);
-    font.draw(batch, "Press Enter to return to main menu", 250, 200);
-    font.draw(batch, "Press Delete to return to exit", 250, 150);
-    batch.end();
+  private Label createTitleLabel(Skin skin) {
+    Label label = new Label("Game paused", skin);
 
-    if (Gdx.input.isTouched()) {
-      game.setScreen(game.getPreviousScreen());
-      dispose();
-    }
-    if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
-      game.setScreen(new MainMenuScreen(game));
-    }
-    if (Gdx.input.isKeyPressed(Input.Keys.DEL)) {
-      Gdx.app.exit();
-    }
+    // Double font size
+    label.setSize(label.getWidth() * 2, label.getHeight());
+    label.setFontScale(2);
+
+    int width = Gdx.graphics.getWidth();
+    int height = Gdx.graphics.getHeight();
+
+    label.setPosition(
+        (width - label.getWidth()) / 2,
+        (height - label.getHeight()) / 2 + height / 4
+    );
+
+    return label;
+  }
+
+  private Button createButton(Skin skin) {
+    TextButton button = new TextButton("RESUME GAME", skin, "default");
+    int buttonWidth = 300;
+    int buttonHeight = 60;
+    button.setWidth(buttonWidth);
+    button.setHeight(buttonHeight);
+
+    int width = Gdx.graphics.getWidth();
+    int height = Gdx.graphics.getHeight();
+
+    button.setPosition(
+        (width - buttonWidth) / 2,
+        (height - buttonHeight) / 2 - height / 4
+    );
+
+    button.addListener(new ClickListener() {
+      @Override
+      public void clicked(InputEvent event, float xPos, float yPos) {
+        game.setScreen(game.getPreviousScreen());
+        dispose();
+      }
+    });
+
+    return button;
   }
 }
