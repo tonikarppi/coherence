15f60b976d42bead28b155506df38cf8436b21dc
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-21 14:06:24 +0200

    Change MenuScreen to use tables

--- core/src/com/pqbyte/coherence/MenuScreen.java
+++ core/src/com/pqbyte/coherence/MenuScreen.java
@@ -1,20 +1,17 @@
 package com.pqbyte.coherence;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
@@ -22,20 +19,34 @@ public class MenuScreen extends ScreenAdapter {
 
   private Stage stage;
   private Coherence game;
-  Texture spaceShipImage;
 
+  /**
+   * The main menu screen.
+   *
+   * @param game The game instance.
+   */
   public MenuScreen(Coherence game) {
     this.game = game;
     Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-    Button button = createButton(skin);
-    Label titleLabel = createTitleLabel(skin);
-
+    Table table = new Table();
+    table.setFillParent(true);
     stage = new Stage();
-    stage.addActor(button);
-    stage.addActor(titleLabel);
+    stage.addActor(table);
 
-    spaceShipImage = new Texture(Gdx.files.internal("Spaceship.jpg"));
+    Label titleLabel = createTitleLabel(skin);
+    table.add(titleLabel).spaceBottom(20);
+    table.row();
 
+    int imageHeight = 300;
+    int imageWidth = (int) (imageHeight * 1.34);
+    Image image = new Image(new Texture(Gdx.files.internal("Spaceship.jpg")));
+    table.add(image).width(imageWidth).height(imageHeight).spaceBottom(100);
+    table.row();
+
+    Button button = createButton(skin);
+    table.add(button).width(300).height(60);
+
+    table.setDebug(Constants.isDebug());
     Gdx.input.setInputProcessor(stage);
   }
 
@@ -43,10 +54,6 @@ public class MenuScreen extends ScreenAdapter {
   public void render(float delta) {
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    SpriteBatch batch = game.batch;
-    batch.begin();
-    batch.draw(spaceShipImage,500,300);
-    batch.end();
     stage.draw();
   }
 
@@ -62,31 +69,15 @@ public class MenuScreen extends ScreenAdapter {
     label.setSize(label.getWidth() * 2, label.getHeight());
     label.setFontScale(2);
 
-    int width = Gdx.graphics.getWidth();
-    int height = Gdx.graphics.getHeight();
-
-    label.setPosition(
-        (width - label.getWidth()) / 2,
-        (height - label.getHeight()) / 2 + height / 4
-    );
-
     return label;
   }
 
   private Button createButton(Skin skin) {
-    TextButton button = new TextButton("START GAME", skin, "default");
+    TextButton button = new TextButton("START GAME", skin);
     int buttonWidth = 300;
     int buttonHeight = 60;
     button.setWidth(buttonWidth);
     button.setHeight(buttonHeight);
-
-    int width = Gdx.graphics.getWidth();
-    int height = Gdx.graphics.getHeight();
-
-    button.setPosition(
-        (width - buttonWidth) / 2,
-        (height - buttonHeight) / 2 - height / 4
-    );
 
     button.addListener(new ClickListener() {
       @Override
