768afc62ca822e45865b7123e141a2898ad89a60
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-22 10:47:57 +0200

    Add Hud class

---
+++ core/src/com/pqbyte/coherence/Hud.java
@@ -0,0 +1,73 @@
+package com.pqbyte.coherence;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import javafx.scene.control.Tab;
+
+public class Hud {
+
+  private Stage stage;
+  private Touchpad movementController;
+  private Touchpad shooterController;
+
+  /**
+   * The overlay displayed over the game.
+   *
+   * @param batch The drawing batch.
+   */
+  public Hud(Batch batch) {
+    float screenWidth = Gdx.graphics.getWidth();
+    float screenHeight = Gdx.graphics.getHeight();
+
+    Viewport viewport = new FillViewport(
+        screenWidth,
+        screenHeight
+    );
+
+    stage = new Stage(viewport, batch);
+    movementController = makeController();
+    shooterController = makeController();
+
+    Table bottomTable = new Table();
+    bottomTable.setFillParent(true);
+    bottomTable.bottom().padBottom(10).padLeft(30).padRight(30);
+    bottomTable.add(movementController);
+    bottomTable.add().expandX();
+    bottomTable.add(shooterController);
+    bottomTable.setDebug(true);
+    stage.addActor(bottomTable);
+  }
+
+  private Touchpad makeController() {
+    Skin skin = new Skin();
+    skin.add("background", new Texture("touchBackground.png"));
+    skin.add("knob", new Texture("touchKnob.png"));
+    Drawable background = skin.getDrawable("background");
+    Drawable knob = skin.getDrawable("knob");
+    Touchpad.TouchpadStyle style = new Touchpad.TouchpadStyle();
+    style.background = background;
+    style.knob = knob;
+
+    return new Touchpad(10, style);
+  }
+
+  public Stage getStage() {
+    return stage;
+  }
+
+  public Touchpad getMovementController() {
+    return movementController;
+  }
+
+  public Touchpad getShooterController() {
+    return shooterController;
+  }
+}
