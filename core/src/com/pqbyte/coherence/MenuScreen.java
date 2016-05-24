0b85bc24891e5ef7f9f285a02ef7151bd7fbf46f
Toni Karppi <toni.pqbyte@gmail.com>
2016-05-24 13:13:12 +0200

    Format MenuScreen according to Google guidelines

--- core/src/com/pqbyte/coherence/MenuScreen.java
+++ core/src/com/pqbyte/coherence/MenuScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -27,12 +26,13 @@ public class MenuScreen extends ScreenAdapter {
    */
   public MenuScreen(Coherence game) {
     this.game = game;
-    Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
     Table table = new Table();
     table.setFillParent(true);
     stage = new Stage();
     stage.addActor(table);
 
+    Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
     Label titleLabel = createTitleLabel(skin);
     table.add(titleLabel).spaceBottom(20);
     table.row();
@@ -81,7 +81,7 @@ public class MenuScreen extends ScreenAdapter {
 
     button.addListener(new ClickListener() {
       @Override
-      public void clicked(InputEvent event, float xPos, float yPos) {
+      public void clicked(InputEvent event, float xpos, float ypos) {
         game.setScreen(new GameScreen(game));
         dispose();
       }
